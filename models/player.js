8c3a22ebabff0c2aa5de84381c591e094ee5baf2
thom page <thom.page.000@gmail.com>
2017-05-29 18:07:22 -0400

    refactors player model

--- models/player.js
+++ models/player.js
@@ -1,13 +1,10 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-var playerSchema = mongoose.Schema({
-
-	name: { type: String, required: true },
-	avatar: { type: String, required: true },
-	player_since: { type: String, required: true },
-	userName: { type: String, required: true }
-
+const playerSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  avatar: { type: String, required: true },
+  player_since: { type: String, required: true },
+  userName: { type: String, required: true }
 });
-
 
 module.exports = mongoose.model('Player', playerSchema);

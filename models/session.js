ececb9b6d4f11d87a8d018e914049952e05fa739
thom page <thom.page.000@gmail.com>
2017-05-29 18:09:14 -0400

    refactors sessions model

--- models/session.js
+++ models/session.js
@@ -1,23 +1,21 @@
-var mongoose = require('mongoose');
-var gameSchema = require('./game').schema;
+const mongoose = require('mongoose');
+const gameSchema = require('./game').schema;
 
-var sessionSchema = mongoose.Schema({
-
-	date: { type: Date, required: true },
-	game: {
-					type: mongoose.Schema.Types.ObjectId,
-					ref: 'Game',
-					required: true
-	      },
-	scores: [
-					 {
-					 	player: { type: mongoose.Schema.Types.ObjectId, ref: 'Player', required: true },
-					 	score: { type: Number, required: true },
-					 	winner: { type: Boolean, default: false }
-					 }
-				 ],
-	userName: { type: String, required: true }
-
+const sessionSchema = mongoose.Schema({
+  date: { type: Date, required: true },
+  game: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Game',
+          required: true
+        },
+  scores: [
+            {
+              player: { type: mongoose.Schema.Types.ObjectId, ref: 'Player', required: true },
+              score: { type: Number, required: true },
+              winner: { type: Boolean, default: false }
+            }
+          ],
+  userName: { type: String, required: true }
 });
 
 module.exports = mongoose.model('Session', sessionSchema);

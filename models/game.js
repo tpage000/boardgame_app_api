5b2d05a82ef21d19e1923cbed591758c885253b6
thom page <thom.page.000@gmail.com>
2017-05-29 18:06:37 -0400

    refactors game model

--- models/game.js
+++ models/game.js
@@ -1,25 +1,24 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-var gameSchema = mongoose.Schema({
-
-	name: String,
-	gameId: { type: Number, required: true },
-	isExpansion: { type: Boolean, default: false },
-	expands: {},
-	minPlayers: { type: Number, default: 1 },
-	maxPlayers: { type: Number, default: 1 },
-	image: { type: String, default: "https://image.freepik.com/free-icon/white-question-mark-on-a-black-circular-background_318-35996.jpg" },
-	thumbnail: { type: String, default: "http://image.flaticon.com/icons/png/128/25/25400.png" },
-	mechanics: Array,
-	designers: Array,
-	artists: Array,
-	publishers: Array,
-	yearPublished: Number,
-	acquired: { type: Date, required: true },
-	inCollection: { type: Boolean, default: true },
-	plays: { type: Number, default: 0 },
-	expansions: Array,
-	userName: { type: String, required: true }
+const gameSchema = mongoose.Schema({
+  name: String,
+  gameId: { type: Number, required: true },
+  isExpansion: { type: Boolean, default: false },
+  expands: {},
+  minPlayers: { type: Number, default: 1 },
+  maxPlayers: { type: Number, default: 1 },
+  image: { type: String, default: "https://image.freepik.com/free-icon/white-question-mark-on-a-black-circular-background_318-35996.jpg" },
+  thumbnail: { type: String, default: "http://image.flaticon.com/icons/png/128/25/25400.png" },
+  mechanics: Array,
+  designers: Array,
+  artists: Array,
+  publishers: Array,
+  yearPublished: Number,
+  acquired: { type: Date, required: true },
+  inCollection: { type: Boolean, default: true },
+  plays: { type: Number, default: 0 },
+  expansions: Array,
+  userName: { type: String, required: true }
 });
 
 module.exports = mongoose.model('Game', gameSchema);

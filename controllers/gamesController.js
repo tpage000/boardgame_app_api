d9e82e596f6474e9e758fd77cd87ca808883a18a
thom page <thom.page.000@gmail.com>
2017-05-29 18:32:56 -0400

    refactors with arrow functions

--- controllers/gamesController.js
+++ controllers/gamesController.js
@@ -4,9 +4,8 @@ const request = require('request');
 const Game = require('../models/game');
 const Session = require('../models/session');
 
-
 // index
-router.get('/', function(req, res) {
+router.get('/', (req, res) => {
   let searchName;
   if (!req.session.loggedInUser) {
     searchName = "example"
@@ -14,25 +13,25 @@ router.get('/', function(req, res) {
     searchName = req.session.loggedInUser.username;
   }
   console.log("Finding all games for: ", searchName);
-  Game.find({ userName: searchName }).sort({date: 'descending'}).exec(function(err, games) {
+  Game.find({ userName: searchName }).sort({date: 'descending'}).exec((err, games) => {
     if (err) throw err;
     res.json(games);
   });
 });
 
 // show
-router.get('/:id', function(req, res) {
+router.get('/:id', (req, res) => {
   console.log('id param: ', req.params.id);
-  Game.findById(req.params.id, function(err, foundGame) {
+  Game.findById(req.params.id, (err, foundGame) => {
     if (err) throw err;
 
     if (foundGame.isExpansion) {
-      Game.find({ 'gameId' : foundGame.expands[0].gameId }, 'name id plays', function(experr, parentGames) {
+      Game.find({ 'gameId' : foundGame.expands[0].gameId }, 'name id plays', (experr, parentGames) => {
         foundGame.expansions = parentGames;
         res.json(foundGame);
       });
     } else {
-      Game.find({ 'expands.gameId': foundGame.gameId}, 'name id plays', function(err, expansions) {
+      Game.find({ 'expands.gameId': foundGame.gameId}, 'name id plays', (err, expansions) => {
         console.log('expansions: ', expansions);
           foundGame.expansions = expansions;
           res.json(foundGame);
@@ -42,12 +41,12 @@ router.get('/:id', function(req, res) {
 });
 
 // create
-router.post('/', function(req, res) {
+router.post('/', (req, res) => {
   if (!req.session.loggedInUser.username) {
     res.send({status: 401, message: "Unauthorized"});
   } else {
     req.body.userName = req.session.loggedInUser.username;
-    Game.create(req.body, function(err, newGame) {
+    Game.create(req.body, (err, newGame) => {
       if (err) {
         console.log('Create game Error: ', err);
         res.json(err);

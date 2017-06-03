a5344fbb4ecc47d02b836416a5fb5a9a7d2d673c
Thom Page <thom.page.000@gmail.com>
2017-06-03 16:16:14 -0400

    tidies up games controller

--- controllers/gamesController.js
+++ controllers/gamesController.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const Game = require('../models/game');
-const Session = require('../models/session');
-const jwt = require('jsonwebtoken');
 const exampleGames = require('../data/exampleGames');
 
 // index of user's games -- req.user set by auth middleware

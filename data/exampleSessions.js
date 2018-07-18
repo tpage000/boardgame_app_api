91a741099bd8718d43952e52516b4c322d1e245b
thom page <thom.page.000@gmail.com>
2018-07-17 20:11:32 -0400

    Replace bcrypt with bcrypt-nodejs
    * Tidyup example data

--- data/exampleSessions.js
+++ data/exampleSessions.js
@@ -1,27 +1,47 @@
 module.exports = [
-  { _id: 1,
-    game: { name: '', thumbnail: '' },
+  { 
+    _id: 1,
+    game: { name: 'Patchwork', thumbnail: 'https://cf.geekdo-images.com/thumb/img/3KaofJ_Nl0a7lOFXS9PveSmQ7vE=/fit-in/200x150/pic2270442.jpg' },
     date: '2016-06-30T04:00:00.000Z',
-    gameresults: [ { player: { name: '', avatar: '' }, score: 6 }, { player: { name: '', avatar: '' }, score: 6 } ],
-  },
-  { _id: 2,
-    game: { name: '', thumbnail: '' },
-    date: '2016-08-05T04:00:00.000Z',
-    gameresults: [ { player: { name: '', avatar: '' }, score: 6 }, { player: { name: '', avatar: '' }, score: 6 } ],
-  },
-  { _id: 3,
-    game: { name: '', thumbnail: '' },
-    date: '2016-08-06T05:00:00.000Z',
-    gameresults: [ { player: { name: '', avatar: '' }, score: 6 }, { player: { name: '', avatar: '' }, score: 6 } ],
-  },
-  { _id: 4,
-    game: { name: '', thumbnail: '' },
-    date: '2016-08-06T04:00:00.000Z',
-    gameresults: [ { player: { name: '', avatar: '' }, score: 6 }, { player: { name: '', avatar: '' }, score: 6 } ],
-  },
-  { _id: 5,
-    game: { name: '', thumbnail: '' },
-    date: '2016-08-07T04:00:00.000Z',
-    gameresults: [ { player: { name: '', avatar: '' }, score: 6 }, { player: { name: '', avatar: '' }, score: 6 } ],
+    gameresults: [ 
+      { 
+        player: { 
+          kind: 'Guest', 
+          info: { 
+            username: 'pizza rat', avatar: '/assets/avatars/pizza-rat.jpg' 
+          }
+        }, 
+        score: 6 
+      },
+      { 
+        player: { 
+          kind: 'Guest', 
+          info: { 
+            username: 'xena', avatar: '/assets/avatars/xena.jpg' 
+          }
+        }, 
+        score: 6 
+      }
+    ]
   }
+  // { _id: 2,
+  //   game: { name: '', thumbnail: '' },
+  //   date: '2016-08-05T04:00:00.000Z',
+  //   gameresults: [ { player: { name: '', avatar: '' }, score: 6 }, { player: { name: '', avatar: '' }, score: 6 } ],
+  // },
+  // { _id: 3,
+  //   game: { name: '', thumbnail: '' },
+  //   date: '2016-08-06T05:00:00.000Z',
+  //   gameresults: [ { player: { name: '', avatar: '' }, score: 6 }, { player: { name: '', avatar: '' }, score: 6 } ],
+  // },
+  // { _id: 4,
+  //   game: { name: '', thumbnail: '' },
+  //   date: '2016-08-06T04:00:00.000Z',
+  //   gameresults: [ { player: { name: '', avatar: '' }, score: 6 }, { player: { name: '', avatar: '' }, score: 6 } ],
+  // },
+  // { _id: 5,
+  //   game: { name: '', thumbnail: '' },
+  //   date: '2016-08-07T04:00:00.000Z',
+  //   gameresults: [ { player: { name: '', avatar: '' }, score: 6 }, { player: { name: '', avatar: '' }, score: 6 } ],
+  // }
 ];

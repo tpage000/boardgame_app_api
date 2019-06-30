fc6e1a3b6ba8254c6a25135a8a274fd2d2aea5f3
thom page <thom.page.000@gmail.com>
2019-06-30 18:30:03 -0400

    Remove duplicate auth middleware file

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -1,19 +1,16 @@
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 
 function authorize (req, res, next) {
-  const token = req.headers['x-access-token']
-  if (!token) {
-    return next('Not Authorized');
-  } else {
+  const token = req.headers['x-access-token'];
+  if (token) {
     jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
-      if (err) {
-        return next();
-      } else {
-        req.user = decodedToken;
-        next();
-      }
-    });
+      if (err) return next(err);
+      req.user = decodedToken;
+      next();
+    })
+  } else {
+    return next('Not Authorized')
   }
 }
 
-module.exports = authorize
+module.exports = authorize;

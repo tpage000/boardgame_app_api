ada8a6106426c2336937eab5d35540b9226e1930
thom page <thom.page.000@gmail.com>
2019-06-30 18:15:49 -0400

    Change order of routes in controller

--- controllers/gamesController.js
+++ controllers/gamesController.js
@@ -23,6 +23,40 @@ router.get('/', (req, res) => {
   }
 });
 
+// show a game and its related sessions
+router.get('/:id', (req, res) => {
+  Game.findById(req.params.id, (err, foundGame) => {
+    if (err) {
+      res.status(400).send({ message: err.message });
+    }
+    Session.find({ game: req.params.id}, null, {sort: '-date'}, (err, sessions) => {
+      if (err) throw err;
+      const opts = [{ path: 'gameresults.player.info', select: 'username avatar'}]
+      const promise = Session.populate(sessions, opts)
+      promise.then((gameSessions) => {
+        res.json({ game: foundGame, sessions: gameSessions });
+      });
+    });
+  });
+});
+
+// create a new game belonging to the user -- req.user comes from auth
+// middleware
+router.post('/', (req, res) => {
+  if (!req.user) {
+    res.status(401).send({ message: "Unauthorized" });
+  } else {
+    req.body.user_id = req.user.id;
+    Game.create(req.body, (err, newGame) => {
+      if (err) {
+        res.status(400).send({ message: err.message });
+      } else {
+        res.status(200).json(newGame);
+      }
+    });
+  }
+});
+
 // search external API for a set of matching title results
 router.get('/search', async (req, res) => {
   let title = req.query.title;
@@ -52,23 +86,6 @@ router.get('/search', async (req, res) => {
   }
 });
 
-// show a game and its related sessions
-router.get('/:id', (req, res) => {
-  Game.findById(req.params.id, (err, foundGame) => {
-    if (err) {
-      res.status(400).send({ message: err.message });
-    }
-    Session.find({ game: req.params.id}, null, {sort: '-date'}, (err, sessions) => {
-      if (err) throw err;
-      const opts = [{ path: 'gameresults.player.info', select: 'username avatar'}]
-      const promise = Session.populate(sessions, opts)
-      promise.then((gameSessions) => {
-        res.json({ game: foundGame, sessions: gameSessions });
-      });
-    });
-  });
-});
-
 // query an external API for extended details of a single game
 router.get('/query/:id', async (req, res) => {
   let options = {
@@ -82,22 +99,5 @@ router.get('/query/:id', async (req, res) => {
     res.status(400).send({ message: err.message })
   }
 })
-
-// create a new game belonging to the user -- req.user comes from auth
-// middleware
-router.post('/', (req, res) => {
-  if (!req.user) {
-    res.status(401).send({ message: "Unauthorized" });
-  } else {
-    req.body.user_id = req.user.id;
-    Game.create(req.body, (err, newGame) => {
-      if (err) {
-        res.status(400).send({ message: err.message });
-      } else {
-        res.status(200).json(newGame);
-      }
-    });
-  }
-});
 
 module.exports = router;
